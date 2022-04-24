ad808bde1ae9cb93c456bd1e9af4935db14e2fc1
trantinvan21087 <74363485+trantinvan21087@users.noreply.github.com>
2022-04-24 17:05:11 +0700

    working on render data
    +building template tag
    +output dynamic data

--- crm1/accounts/queryDemos.py
+++ crm1/accounts/queryDemos.py
@@ -34,6 +34,7 @@ productsFiltered = Product.objects.filter(tags__name="Sports")
 '''
 (11)Bonus
 Q: If the customer has more than 1 ball, how would you reflect it in the database?
+  
 A: Because there are many different products and this value changes constantly you would most 
 likly not want to store the value in the database but rather just make this a function we can run
 each time we load the customers profile
@@ -59,7 +60,7 @@ class ParentModel(models.Model):
 	name = models.CharField(max_length=200, null=True)
 
 class ChildModel(models.Model):
-	parent = models.ForeignKey(Customer)
+	parent = models.ForeignKey(ParentModel)
 	name = models.CharField(max_length=200, null=True)
 
 parent = ParentModel.objects.first()
