a4317ff7c404057d7a6aa34e2de26b1af22fbc56
trantinvan21087 <74363485+trantinvan21087@users.noreply.github.com>
2022-04-24 19:54:30 +0700

    extend user model (relationship)
    adding a relationship to other models

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -28,6 +28,11 @@ def registerPage(request):
 
 			group = Group.objects.get(name='customer')
 			user.groups.add(group)
+			#Added username after video because of error returning customer name if not added
+			Customer.objects.create(
+				user=user,
+				name=user.username,
+				)
 
 			messages.success(request, 'Account was created for ' + username)
 
@@ -77,8 +82,19 @@ def home(request):
 
 	return render(request, 'accounts/dashboard.html', context)
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-	context = {}
+	orders = request.user.customer.order_set.all()
+
+	total_orders = orders.count()
+	delivered = orders.filter(status='Delivered').count()
+	pending = orders.filter(status='Pending').count()
+
+	print('ORDERS:', orders)
+
+	context = {'orders':orders, 'total_orders':total_orders,
+	'delivered':delivered,'pending':pending}
 	return render(request, 'accounts/user.html', context)
 
 
